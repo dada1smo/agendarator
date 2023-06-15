43d4eb6f90c2b9485ceab4517a02cb9597461296
Adalberto Sampaio <sampaio.afn@gmail.com>
2023-06-15 20:56:33 -0300

    fix home page link

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h2>A5</h2>
       <div className="linkRow">
         <Link href="/agenda/a5/2023">Agenda 2023</Link>
-        <Link href="/agenda/a5/2023">Agenda 2024</Link>
+        <Link href="/agenda/a5/2024">Agenda 2024</Link>
       </div>
       <h2>A4</h2>
       <h3>1 coluna</h3>
