e10e1d79c8b5de980c42fa76b7687187428a2013
Adalberto Sampaio <sampaio.afn@gmail.com>
2023-06-21 13:50:56 -0300

    feat: Automatically creates page index

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,59 @@
 import Link from 'next/link';
+import fs from 'fs';
 
-export default function Home() {
+export async function getStaticProps() {
+  const files = fs.readdirSync('public/years');
+  const years = files.map((fileName) => fileName.replace('.json', ''));
+  return {
+    props: {
+      years,
+    },
+  };
+}
+
+export default function Home({ years }) {
   return (
     <div className="container">
       <h1>Agendas</h1>
       <h2>A5</h2>
       <div className="linkRow">
-        <Link href="/agenda/a5/2023">Agenda 2023</Link>
-        <Link href="/agenda/a5/2024">Agenda 2024</Link>
+        {years.map((y) => {
+          return <Link key={y} href={`/agenda/a5/${y}`}>{`Agenda ${y}`}</Link>;
+        })}
       </div>
       <h2>A4</h2>
       <h3>1 coluna</h3>
       <div className="linkRow">
-        <Link href="/agenda/a4/1coluna/2023">Agenda 2023</Link>
-        <Link href="/agenda/a4/1coluna/2024">Agenda 2024</Link>
+        {years.map((y) => {
+          return (
+            <Link
+              key={y}
+              href={`/agenda/a4/1coluna/${y}`}
+            >{`Agenda ${y}`}</Link>
+          );
+        })}
       </div>
       <h4>Cientista Arteira</h4>
       <div className="linkRow">
-        <Link href="/agenda/a4/1coluna/2023">Agenda 2023</Link>
-        <Link href="/agenda/a4/1coluna/2024">Agenda 2024</Link>
+        {years.map((y) => {
+          return (
+            <Link
+              key={y}
+              href={`/agenda/a4/1coluna/cientista-arteira/${y}`}
+            >{`Agenda ${y}`}</Link>
+          );
+        })}
       </div>
       <h3>2 colunas</h3>
       <div className="linkRow">
-        <Link href="/agenda/a4/2colunas/2023">Agenda 2023</Link>
-        <Link href="/agenda/a4/2colunas/2024">Agenda 2024</Link>
+        {years.map((y) => {
+          return (
+            <Link
+              key={y}
+              href={`/agenda/a4/2coluna/${y}`}
+            >{`Agenda ${y}`}</Link>
+          );
+        })}
       </div>
       <style jsx>{`
         .linkRow {
